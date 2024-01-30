9f489bef1101e410cb4db9e5ad4d24a5d0c6e8c6
Felipe D S MArtins <37319545+TecFelipeDsMartins@users.noreply.github.com>
2024-01-30 01:30:55 +0000

    add a new favicon

--- pages/[[...catchall]].jsx
+++ pages/[[...catchall]].jsx
@@ -10,6 +10,8 @@ import Error from "next/error";
 import { useRouter } from "next/router";
 import { PLASMIC } from "../plasmic-init";
 
+
+
 export default function PlasmicLoaderPage(props) {
   const { plasmicData, queryCache } = props;
   const router = useRouter();
